1ba2a005e35daf254522479c222deed8a225a707
enriqTS <henrcraft@gmail.com>
2021-11-16 10:25:47 -0300

    curso node

---
+++ JS/mission.js
@@ -0,0 +1,8 @@
+const mission = process.argv[2];
+
+if (mission === 'learn'){
+    console.log('Time to write some code');
+}
+else{
+    console.log(`Is ${mission} really more fun?`)
+}
---
+++ JS/race.js
@@ -0,0 +1,4 @@
+setTimeout(() => console.log('Rabbit finishes'), 1000);
+
+//console.log('Rabbit finishes');
+console.log('Turtle finishes');
