626be644fa4f2880ddcdc1ee25811f15b618389e
enriqTS <henrcraft@gmail.com>
2022-02-11 14:48:59 -0300

    s

--- JS/packages-example/request.js
+++ JS/packages-example/request.js
@@ -1,1 +1,12 @@
 const axios = require('axios');
+
+axios.get('https://www.google.com')
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .then(() => {
+        console.log('All done')
+    });
