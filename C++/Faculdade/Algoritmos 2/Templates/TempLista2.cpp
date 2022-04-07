7ee13a4759e8bcb066cf66907d680a74447a83fc
enriqTS <henrcraft@gmail.com>
2022-04-07 08:24:35 -0300

    s

--- C++/Faculdade/Algoritmos 2/Templates/TempLista2.cpp
+++ C++/Faculdade/Algoritmos 2/Templates/TempLista2.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 
 template <typename T>
-void swap( T data[], T data1[])
-{
-    T tmp[] = data[];
-    data[] = data2[];
-    data2[] = tmp[];
-}
-
-template <typename T>
 void selectionsort( T data[], int n)
 {
     int j, least;
     for(int i = 0; i < n-1; i++)
     {
         for(j = i+1, least=i; j<n; j++)
-            if(data[j < data[least]])
+            if(data[j] < data[least])
                 least = j;
-        swap(data[least], data[i]);        
+        std::swap(data[least], data[i]);        
     }
 } 
 
 int main()
 {
     int Data[5] = {5,1,2,3,4};
-    float Data1[5] = {6.6,7.7,9.9,8.8,10.10};
+    float Data1[5] = {7.7,6.6,9.9,8.8,10.10};
     for(int i = 0; i < 5; i++)
     {
         std::cout << "Itens do vetor: " << Data[i] << "\n";
@@ -33,5 +25,16 @@ int main()
     for(int i = 0; i < 5; i++)
     {
         std::cout << "Itens do vetor(Organizados): " << Data[i] << "\n";
-    }    
+    }   
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << "Itens do vetor: " << Data1[i] << "\n";
+    }
+    selectionsort(Data1, 5);
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << "Itens do vetor(Organizados): " << Data1[i] << "\n";
+    }   
+    
+    return 0;
 }
