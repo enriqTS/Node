b2d1f7a71246c787c4aa9c650e312c1fa6c4fff0
enriqTS <henrcraft@gmail.com>
2022-05-20 22:41:36 -0300

    s

--- C++/Faculdade/Algoritmos 2/TrabalhoP1/main.cpp
+++ C++/Faculdade/Algoritmos 2/TrabalhoP1/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cstring>
+
 using namespace std;
 #define MAXCHAR 26
 
@@ -13,22 +14,20 @@ void processingdata(string &T){
         }
     }    
     for(int i=0; i<=m;i++){
-        if(!isprint(T[i])){
-            if(T[i] >=192 && T[i] <= 198 || T[i] >=224 && T[i] <= 230){
-                chartoascii = T[i];
-                chartoascii = 97;
-                T[i] = chartoascii;
+        if(T[i] >=-128 && T[i] <= 0){
+            if(T[i] >=-128 && T[i] <= -122 || T[i] >=-96 && T[i] <= -91){
+                T[i] = 'a';
             }
-            if(T[i] >=200 && T[i] <= 203 && T[i] >=232 && T[i] <= 235){
+            else if(T[i] >=-120 && T[i] <= -117 && T[i] >=-88 && T[i] <= -85){
                 T[i] = 'e';
             }
-            if(T[i] >=204 && T[i] <= 207 && T[i] >=236 && T[i] <= 239){
+            else if(T[i] >=204 && T[i] <= 207 && T[i] >=236 && T[i] <= 239){
                 T[i] = 'i';
             }
-            if(T[i] >=210 && T[i] <= 242 && T[i] >=224 && T[i] <= 246){
+            else if(T[i] >=210 && T[i] <= 242 && T[i] >=224 && T[i] <= 246){
                 T[i] = 'o';
             }
-            if(T[i] == 199 && T[i] == 231){
+            else if(T[i] == -89 && T[i] == -121){
                 T[i] = 'c';
             }
         }
@@ -39,12 +38,21 @@ void processingdata(string &T){
             T[i] = ' ';
         }
     }
+
+    for(int i=0; i<m; i++){
+        if(!isprint(T[i]))
+            T[i] = '\0';
+    }
 }
 
 int main(){
     // char lowerc[MAXCHAR] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     // char upperc[MAXCHAR] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    string T = "AJGDSAYID!,.#@áÁÉé";
+    string T = "AJGDSAYID!,.#@áÁÉéçÇ";
+
+    char a = 'ë';
+    int x = a;
+    cout << x << endl;
     
     processingdata(T);
     cout << T << endl;
--- C++/Faculdade/Algoritmos 2/codigosProcessamentoCaracteres/forcaBruta.cpp
+++ C++/Faculdade/Algoritmos 2/codigosProcessamentoCaracteres/forcaBruta.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 #include <bitset>
 #define MAXCHAR 26
 using namespace std;
 
 void ForcaBruta(string T, int n, string P, int m)
-{  int i , j , k;
-   for (i = 1; i <= (n -m + 1); i++)
+{  int i , j , k, x;
+   for (i = 1; i <= (n - m + 1); i++)
    {
      k = i ; j = 1;
      while(T[k-1] == P[ j-1] && j <=m)
@@ -14,7 +14,23 @@ void ForcaBruta(string T, int n, string P, int m)
         j ++; k++;
      }
      if(j>m)
-        cout<<"Casamento na posicao "<<i<<endl;
+        cout<<"Casamento na posicao1 "<<i<<endl;
+     else
+        cout<<T[k-1]<<" <> "<<P[j-1]<<endl;
+    cout<<endl;
+   }
+   for (i = 1; i <= (n - m + 1); i++)
+   {
+     k = i ; j = 1; x = m- 1;
+     while(T[k-1] == P[j-1] && j <=m)
+     {
+        cout<<T[k-1]<<" == "<<P[j-1]<<endl;
+        j ++; k++;
+     }
+     if(j==x)
+         cout << "Casamento aproximado na posicao " << endl;
+     if(j>m)
+        cout<<"Casamento na posicao2 "<<i<<endl;
      else
         cout<<T[k-1]<<" <> "<<P[j-1]<<endl;
     cout<<endl;
@@ -23,8 +39,10 @@ void ForcaBruta(string T, int n, string P, int m)
 
 int main()
 {
-    string T="ostestestesta";
+
+    string T="ostestestestes";
     string P="teste";
+    P.length(j-1) = 'a';
     int m=P.size(),n=T.size();
     ForcaBruta(T, n, P, m);
     return 0;
